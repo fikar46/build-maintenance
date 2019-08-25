89d6f2fc6563799fa584a4e335e11da3bd059620
fikar46 <43537558+fikar46@users.noreply.github.com>
2019-08-25 14:39:27 +0700

    first

---
+++ precache-manifest.c2fdbc3941add010479ea29cee36dd95.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "e4def5c720360a32d400",
+    "url": "/static/css/main.af3d6248.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd0baf11f69a8ddd65e844a859fcbd06",
+    "url": "/static/media/japan.bd0baf11.jpeg"
+  },
+  {
+    "revision": "bc93979a870fc1aecd93",
+    "url": "/static/js/2.cfd7a3c6.chunk.js"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/static/media/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "e4def5c720360a32d400",
+    "url": "/static/js/main.2e6de9a5.chunk.js"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "6d4774d4e483e03057f9d09544656b42",
+    "url": "/static/media/fa-regular-400.6d4774d4.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "3897e68864e7fe1fbb468c0220573010",
+    "url": "/static/media/fa-brands-400.3897e688.svg"
+  },
+  {
+    "revision": "3fad11457f70f880252e0ab5c9cee82e",
+    "url": "/static/media/fa-solid-900.3fad1145.svg"
+  },
+  {
+    "revision": "8bfb5f17d4443e9bae55557c0e45b5d4",
+    "url": "/static/media/china.8bfb5f17.jpeg"
+  },
+  {
+    "revision": "ff287550a78d087860a31644498502ed",
+    "url": "/static/media/malaysia.ff287550.jpeg"
+  },
+  {
+    "revision": "450563c71dae7b28f1e45294113b965e",
+    "url": "/static/media/australia.450563c7.jpeg"
+  },
+  {
+    "revision": "b5661af41eb644b01758b1a2f058b769",
+    "url": "/static/media/seoul.b5661af4.jpeg"
+  },
+  {
+    "revision": "b5b4a7f7dd9fdd0034a665ae6544ad04",
+    "url": "/static/media/singapore.b5b4a7f7.jpeg"
+  },
+  {
+    "revision": "bc93979a870fc1aecd93",
+    "url": "/static/css/2.998d143b.chunk.css"
+  },
+  {
+    "revision": "fc7b41620440fe7f0d6e6038956ac713",
+    "url": "/index.html"
+  }
+];
